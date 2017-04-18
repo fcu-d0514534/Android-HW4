08c51168e848cb6d092b3ae7238e47e552b915ec
fuc-d0514534 <D0514534@gmail.com>
2017-04-18 23:10:25 +0800

    Android-HW4 廣播

---
+++ app/src/main/java/com/shang/mybroadcast/MainActivity.java
@@ -0,0 +1,34 @@
+package com.shang.mybroadcast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("ShangLin");
+                String msg = editText.getText().toString();
+                intent.putExtra("MSG", msg);
+                sendBroadcast(intent);
+            }
+        });
+
+    }
+}
